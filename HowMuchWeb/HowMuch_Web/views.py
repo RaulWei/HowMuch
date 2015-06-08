81574b1fc33302a01e1c0f535db9632bd527263a
RaulWei <674877404@qq.com>
2015-06-08 11:03:08 +0800

    Update HowMuch_Web/views.py cab run

--- HowMuchWeb/HowMuch_Web/views.py
+++ HowMuchWeb/HowMuch_Web/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def qScore(request):
+    return HttpResponse('qScore')
+
+def qCourse(request):
+    return HttpResponse('qCourse')
