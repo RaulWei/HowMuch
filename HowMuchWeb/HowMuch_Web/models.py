76d78cd61fc8636951d84b108b9c5d1fb76b961f
RaulWei <674877404@qq.com>
2015-06-09 22:19:02 +0800

    Update models.py

--- HowMuchWeb/HowMuch_Web/models.py
+++ HowMuchWeb/HowMuch_Web/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib import admin
 
 # Create your models here.
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    j_username = models.CharField(max_length=200)
+    j_password = models.CharField(max_length=200)
+
+admin.site.register(User)
