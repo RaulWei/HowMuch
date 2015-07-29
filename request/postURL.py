7c6fe521119a9231350e3337d9a8f4e60035feb3
RaulWei <674877404@qq.com>
2015-07-29 19:18:41 +0800

    Update postURL.py

--- request/postURL.py
+++ request/postURL.py
@@ -7,5 +7,5 @@ import codecs
 f = codecs.open('postText.xml', 'r', 'utf-8')
 content = ''.join(f.readlines())
 f.close()
-res = requests.post('127.0.0.1:8000', data=content.encode('utf-8'))
+res = requests.post('http://127.0.0.1:8000/grdms/grdms/', data=content.encode('utf-8'))
 print(res.text)
