fc5bcf715ac5c0ece0078612d4ddc1c6ec412bb0
RaulWei <674877404@qq.com>
2015-07-31 15:02:19 +0800

    Add replyMsg.py

---
+++ HowMuchWeb/HowMuch_Web/replyMsg.py
@@ -0,0 +1,10 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'weimw'
+
+replyText = "<xml>" \
+            "<ToUserName><![CDATA[%s]]></ToUserName>" \
+            "<FromUserName><![CDATA[%s]]></FromUserName>" \
+            "<CreateTime>%s</CreateTime>" \
+            "<MsgType><![CDATA[%s]]></MsgType>" \
+            "<Content><![CDATA[%s]]></Content>" \
+            "</xml>"
