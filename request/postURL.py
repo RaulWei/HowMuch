0404a29fb9a07ceb94a7b6e5d781d22fd8414085
RaulWei <674877404@qq.com>
2015-07-31 11:23:01 +0800

    Update test url in text

--- request/postURL.py
+++ request/postURL.py
@@ -7,6 +7,6 @@ import codecs
 f = codecs.open('postText.xml', 'r', 'utf-8')
 content = ''.join(f.readlines())
 f.close()
-# res = requests.post('http://127.0.0.1:8000/grdms/grdms/', data=content.encode('utf-8'))
-res = requests.post('http://1.howmuchbit.sinaapp.com/grdms/grdms', data=content.encode('utf-8'))
+res = requests.post('http://127.0.0.1:8000/grdms/grdms/', data=content.encode('utf-8'))
+# res = requests.post('http://1.howmuchbit.sinaapp.com/grdms/grdms', data=content.encode('utf-8'))
 print(res.text)
