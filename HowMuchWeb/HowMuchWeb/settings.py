76550175fbd8e0bc9e548a2b3946ddee54201ee3
RaulWei <674877404@qq.com>
2015-07-31 22:00:57 +0800

    update settings.py

--- HowMuchWeb/HowMuchWeb/settings.py
+++ HowMuchWeb/HowMuchWeb/settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'HowMuchWeb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'HowMuchWeb',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '3306',
     }
 }
 
