f2193938a968f314e46ddf461cfe636e777ac8b5
RaulWei <674877404@qq.com>
2015-08-01 17:11:28 +0800

    update views.py

--- HowMuchWeb/HowMuch_Web/views.py
+++ HowMuchWeb/HowMuch_Web/views.py
@@ -15,7 +15,8 @@ def qScore(request, fromUserName, toUserName):
     # 先判断是否绑定账号
     user = User.objects.filter(openid=toUserName)
     if not user.exists():
-        reply = replyMsg.replyText % (toUserName, fromUserName, str(int(time.time())), 'text', '您未完成账号绑定，请绑定后查询')
+        reply = replyMsg.replyText % (toUserName, fromUserName, str(int(time.time())), 'text',
+                                      '您未完成账号绑定，请绑定后查询，回复“帮助”查看更多')
         return HttpResponse(reply, content_type="application/xml")
 
     # get scores store in res
@@ -24,9 +25,12 @@ def qScore(request, fromUserName, toUserName):
     grdms_root = GrdmsRobot(username, password)
     res, count = '', 0
     for score in grdms_root.query_points():
-        if count != 0:
+        if count == 1:
             res = res + score.contents[7].string + ', ' + \
-                  score.contents[19].string + ', ' + score.contents[23].string + ';\r\n'
+                  score.contents[19].string + ', ' + score.contents[23].string + ';'
+        elif count != 0:
+            res = res + "\n" + score.contents[7].string + ', ' + \
+                  score.contents[19].string + ', ' + score.contents[23].string + ';'
         count += 1
 
     # reply by xml
