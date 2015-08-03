062dac096c4913d3cc059f5a8ac6d733201f6a26
RaulWei <674877404@qq.com>
2015-08-03 15:43:45 +0800

    update views.py

--- HowMuchWeb/HowMuch_Web/views.py
+++ HowMuchWeb/HowMuch_Web/views.py
@@ -5,41 +5,9 @@ from django.template import loader, Context
 from django.http import HttpResponse
 from GrdmsRobot import GrdmsRobot
 from models import User
-import time
-import wechatUtil
-import replyMsg
-
-# Create your views here.
-
-def qScore(request, fromUserName, toUserName):
-    # 先判断是否绑定账号
-    user = User.objects.filter(openid=toUserName)
-    if not user.exists():
-        reply = replyMsg.replyText % (toUserName, fromUserName, str(int(time.time())), 'text',
-                                      '您未完成账号绑定，请绑定后查询，回复“帮助”查看更多')
-        return HttpResponse(reply, content_type="application/xml")
-
-    # get scores store in res
-    username = user[0].j_username
-    password = user[0].j_password
-    grdms_root = GrdmsRobot(username, password)
-    res, count = '', 0
-    for score in grdms_root.query_points():
-        if count == 1:
-            res = res + score.contents[7].string + ', ' + \
-                  score.contents[19].string + ', ' + score.contents[23].string + ';'
-        elif count != 0:
-            res = res + "\n" + score.contents[7].string + ', ' + \
-                  score.contents[19].string + ', ' + score.contents[23].string + ';'
-        count += 1
-
-    # reply by xml
-    reply = replyMsg.replyText % (toUserName, fromUserName, str(int(time.time())), 'text', res)
-    return HttpResponse(reply, content_type="application/xml")
-
-
-def qCourse(request):
-    return HttpResponse('qCourse')
+import utilWeChat
+import utilText
+import utilEvent
 
 
 def bind(request):
@@ -73,54 +41,26 @@ def bind(request):
 
 def grdms(request):
     if request.method == 'GET':
-        if wechatUtil.checkSignature(request):
+        if utilWeChat.checkSignature(request):
             return HttpResponse(request.GET.get(u'echostr'), content_type="text/plain")
     if request.method == 'POST':
-        dictText = wechatUtil.wechatUtil.parseXml(request)
+        dictText = utilWeChat.parseXml(request)
 
         if dictText['MsgType'] == 'text':
             # 文本
             content = dictText['Content']
-            toUserName = dictText['FromUserName']
-            fromUserName = dictText['ToUserName']
             if content == '查询成绩':
-                return qScore(request, fromUserName, toUserName)
+                return utilText.qScore(dictText)
             if content == '查询课表':
-                return qCourse(request)
+                return utilText.qCourse()
             if content == "绑定":
-                exist = User.objects.filter(openid=toUserName)
-                if exist.exists():
-                    reply = replyMsg.replyText % (toUserName, fromUserName, str(int(time.time())), 'text', '您已经完成账号绑定，回复“帮助”查看更多')
-                    return HttpResponse(reply, content_type="application/xml")
-                else:
-                    replyContent = "<a href=\"http://howmuchbit.sinaapp.com/grdms/bind?openID=" + toUserName + "\">绑定账号</a>"
-                    reply = replyMsg.replyText % (toUserName, fromUserName, str(int(time.time())), 'text', replyContent)
-                    return HttpResponse(reply, content_type="application/xml")
+                return utilText.bind(dictText)
             if content == '帮助':
-                replyContent = "您可以回复我们来获取信息：\n回复“绑定”将微信号与教务系统账号绑定\n" \
-                               "回复“查询成绩”查询研究生阶段所有科目成绩\n回复“帮助”获取本条消息"
-                reply = replyMsg.replyText % (toUserName, fromUserName, str(int(time.time())), 'text', replyContent)
-                return HttpResponse(reply, content_type="application/xml")
+                return  utilText.help(dictText)
 
         if dictText['MsgType'] == 'event':
             # 事件
             event = dictText['Event']
             if event == 'subscribe':
                 # 关注
-                fromUserName = dictText['ToUserName']
-                toUserName = dictText['FromUserName']
-                exist = User.objects.filter(openid=toUserName)
-                # 已经绑定账号
-                if exist.exists():
-                    reply = replyMsg.replyText % (toUserName, fromUserName, str(int(time.time())),
-                                                  'text', '您已经完成账号绑定，回复“帮助”查看更多')
-                    return HttpResponse(reply, content_type="application/xml")
-                # 还没绑定账号
-                content = "感谢关注，请先<a href=\"http://howmuchbit.sinaapp.com/grdms/bind?openID=" \
-                          + toUserName + "\">绑定账号</a>\n\n您可以回复我们来获取信息：\n回复“绑定”将微信号与教务系统账号绑定\n" \
-                                         "回复“查询成绩”查询研究生阶段所有科目成绩\n回复“帮助”获取本条消息"
-                reply = replyMsg.replyText % (toUserName, fromUserName, str(int(time.time())), 'text', content)
-                return HttpResponse(reply, content_type="application/xml")
-
-            if event == 'unsubscribe':
-                pass
+                return utilEvent.subscribe(dictText)
